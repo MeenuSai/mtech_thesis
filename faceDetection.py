8d8a994e457d868c611f0f0ed71e1dc5cc7e362d
Meenu Sai <40272408+MeenuSai@users.noreply.github.com>
2018-11-14 11:46:49 +0530

    Update faceDetection.py

--- faceDetection.py
+++ faceDetection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[81]:
 
 
 import dlib
@@ -8,6 +9,7 @@ from PIL import Image
 from skimage import io
 import sys
 img=sys.argv[1]
+
 
 def detect_faces(image):
 
@@ -21,12 +23,50 @@ def detect_faces(image):
 
     return face_frames
 
+# Load image
 img_path = img
 image = io.imread(img_path)
+#image=Image.open(img_path)
+# Detect faces
 detected_faces = detect_faces(image)
+ 
 
+# Crop faces and plot
 for n, face_rect in enumerate(detected_faces):
+    print(n)
     face = Image.fromarray(image).crop(face_rect)
-    cropFileName=str(img[:-4])+'_cropped.png'
-    face.save('processed/'+cropFileName)
-    print('Image Cropped, New File name is: '+ str(cropFileName))
+    face.save("%s.png" % (n+5))
+    
+ 
+# #!/usr/bin/env python
+# # coding: utf-8
+
+
+
+# import dlib
+# from PIL import Image
+# from skimage import io
+# import sys
+# img=sys.argv[1]
+
+# def detect_faces(image):
+
+#     # Create a face detector
+#     face_detector = dlib.get_frontal_face_detector()
+
+#     # Run detector and get bounding boxes of the faces on image.
+#     detected_faces = face_detector(image, 1)
+#     face_frames = [(x.left(), x.top(),
+#                     x.right(), x.bottom()) for x in detected_faces]
+
+#     return face_frames
+
+# img_path = img
+# image = io.imread(img_path)
+# detected_faces = detect_faces(image)
+
+# for n, face_rect in enumerate(detected_faces):
+#     face = Image.fromarray(image).crop(face_rect)
+#     cropFileName=str(img[:-4])+'_cropped.png'
+#     face.save('processed/'+cropFileName)
+#     print('Image Cropped, New File name is: '+ str(cropFileName))
